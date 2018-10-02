74783290f51ae06901f45659d2dd0b56ada11571
Jackwhitener <jackwhitener1@gmail.com>
2018-10-02 11:28:25 -0400

    makes pizza_app actually work

--- pizza_app.rb
+++ pizza_app.rb
@@ -1,5 +1,5 @@
 def pizza(size,crust,sauce,cheese,toppings)
-puts "Your pizza is a #{size} inch pie, with #{crust} crust, #{sauce} sauce, #{cheese} cheese, and #{toppings} on top."
+puts "Your pizza is a #{size} inch pie, with #{crust} crust, #{sauce} sauce, #{cheese} cheese, #{toppings} on top."
 end
 puts "How many inches in diameter is your pizza?"
 size = gets.chomp
@@ -11,4 +11,4 @@ puts "What kind of cheese is on your pizza"
 cheese = gets.chomp
 puts "Which toppings do you want on your pizza?"
 toppings = gets.chomp
-pizza
+pizza(size,crust,sauce,cheese,toppings)
