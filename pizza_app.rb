b5055a1be2fe392204c09e16d814dd01fa21730b
Jackwhitener <jackwhitener1@gmail.com>
2018-10-02 11:25:32 -0400

    adds pizza app, with some progress

---
+++ pizza_app.rb
@@ -0,0 +1,14 @@
+def pizza(size,crust,sauce,cheese,toppings)
+puts "Your pizza is a #{size} inch pie, with #{crust} crust, #{sauce} sauce, #{cheese} cheese, and #{toppings} on top."
+end
+puts "How many inches in diameter is your pizza?"
+size = gets.chomp
+puts "What kind of crust is on your pizza?"
+crust = gets.chomp
+puts "What kind of sauce is on your pizza?"
+sauce = gets.chomp
+puts "What kind of cheese is on your pizza"
+cheese = gets.chomp
+puts "Which toppings do you want on your pizza?"
+toppings = gets.chomp
+pizza
